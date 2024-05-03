791935e6d21a594a1dd43824ff82eb05f4c9669f
Chen Chen <1686388268@qq.com>
2024-05-03 21:31:51 +0800

    CMSL5.4

--- utils/DownloadTaskList.cs
+++ utils/DownloadTaskList.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace CMSL.utils
 {
-    internal class DownloadTaskList
+    public class DownloadTaskList
     {
+        private List<DownloadItem> items;
+        public DownloadTaskList()
+        {
+            items = new List<DownloadItem>();
+        }
+        public void AddItem(DownloadItem item)
+        {
+            foreach (var downloadItem in items)
+            {
+                if (downloadItem != null && item != null) { 
+                    if (downloadItem.id == item.id)
+                    {
+                        throw new Exception("重复ID这是不允许的标签");
+                    }
+                    items.Add(downloadItem);
+                }
+            }
+        }
+        
+
+        public class DownloadItem
+        {
+            public int id;
+            public string name;
+            public string url;
+            public string description;
+        }
     }
 }
